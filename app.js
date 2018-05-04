2bb312de96a528182930ac4df830f3966793b1ca
kudiersky <32683429+kudiersky@users.noreply.github.com>
2018-05-04 17:25:31 +0100

    built function frame, must restart function

--- app.js
+++ app.js
@@ -75,39 +75,19 @@ romanNumeral: 'm'
 
 
 var romanNumeralsArrayFlipped = romanNumeralsArray.slice(0).reverse();    //We flipped the array
-
+let string =''                                                            //build the string to return roman numerals
 
 function RomanNumerate(value){                                            //take in the value as a parameter
+
 
   for (i = 0; i < romanNumeralsArrayFlipped.length; i++) {                //We are itterateing over the length of the array
        if (value/romanNumeralsArrayFlipped[i].decimal >= 1){              //If we can divide the value by the array item returning a value of 1 or above
          value -= romanNumeralsArrayFlipped[i].decimal                    //We take it off
-         let string = romanNumeralsArrayFlipped[i].romanNumeral           //Get the value of this against our array
-         console.log(string)                                              // log that string
-         break                                                            //break to stop it from continuing. For now
-       }
-  }
-}
+         string += romanNumeralsArrayFlipped[i].romanNumeral              //add the roman numerals to a string                                                                        
+        }
+      }
+    }
 
-//Lets test that first value
 
-testArray = [1,2,3,4,5,6,7,8,11,12,111,123,1444,1333,'1234', 222]
-
-testArray.forEach(RomanNumerate)
-
-// i ✔
-// ii ✔
-// iii ✔
-// iv ✔
-// v ✔
-// vi ✔
-// vii ✔
-// viii ✔
-// x ✔
-// x ✔
-// c ✔
-// c ✔
-// m ✔
-// m ✔
-// m ✔ suprised as its a string, cool
-// c ✔
+RomanNumerate(432)
+console.log(string)
