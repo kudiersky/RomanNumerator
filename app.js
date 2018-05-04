02a9a370033db4ab2e58fbb44a1cce0423090841
kudiersky <32683429+kudiersky@users.noreply.github.com>
2018-05-04 14:50:07 +0100

    Back to the drawing board with the Math

--- app.js
+++ app.js
@@ -1,58 +1,65 @@
-//Below we have our array of Objects of 1 to 9.
 
-const romanNumeralsArray = [
-{
-decimal: 1,
-romanNumeral: 'i'
-},
-{
-decimal: 2,
-romanNumeral: 'ii'
-},
-{
-decimal: 3,
-romanNumeral: 'iii'
-},
-{
-decimal: 4,
-romanNumeral: 'iv'
-},
-{
-decimal: 5,
-romanNumeral: 'v'
-},
-{
-decimal: 6,
-romanNumeral: 'vi'
-},
-{
-decimal: 7,
-romanNumeral: 'vii'
-},
-{
-decimal: 8,
-romanNumeral: 'vii'
-},
-{
-decimal: 9,
-romanNumeral: 'viii'
-}
-]
+PROBLEM!!!!!!  We have found that although we can write an if statememnt for all possibilities, it will take up time.
 
-//Let's and an IF statement to constrain in to between 1 and 10 for now and give an error message if not
+Lets look at breaking a number down:
+From reading up on the web. Which I doubt the Romans had. We can start at the largest number and work down.
 
-function decimalToRomanNumeral(decimal){
+Lets do that with 9.
 
-if(decimal > 0 && decimal < 10 && decimal!==null){
-  let message ='';
-  let obj = romanNumeralsArray.find(object => object.decimal === decimal);
-  message = (obj.romanNumeral) //return a object value
-  }
-    else{
-   message = `We cannot convert ${decimal} to Roman Numerals yet, please enter a number between 1 and  10`
-    }
-    return message
-}
+We can start cycle through our array from the largest to the smallest to see what will return a value above or equal to 1.
 
-let response = decimalToRomanNumeral(10)
-console.log(response)
+1000 no 500 no 100 no 50 no 10 no 9 yes 8 yes 7 yes 6 yes 5 yes 4 yes 3 yes 2 yes 1 yes
+
+This tells me that we can start with the highest of our array decimal values to the lowest.
+If we get 1 or more when dividing it by our array decimal value, we take that value away and add that Roman Numeral Value to a string.
+
+lets look at 128.
+
+Can we divide 128 by decimalvalue, giving 1 or more:
+
+1000 no
+500 no
+100 yes **************
+
+So lets take it off, add to to a string (100 = C). String = C. And lets go again.
+
+28
+
+Can we divide 28 by decimalvalue, giving 1 or more:
+
+1000 no
+500 no
+100 no
+50 no
+10 yes ******************
+
+So lets take it off, add to to a string (10 = X). String = CX. And lets go again.
+
+Can we divide 18 by decimalvalue, giving 1 or more:
+
+1000 no
+500 no
+100 no
+50 no
+10 yes ********************
+
+So lets take it off, add to to a string (10 = X). String = CXX. And lets go again.
+
+Can we divide 8 by decimalvalue, giving 1 or more:
+
+1000 no
+500 no
+100 no
+50 no
+10 no
+9 no
+8 yes ******************
+So lets take it off, add to to a string (8 = viii). String = CXXVIII.
+
+AND HERE WE HAVE 128 in its most Roman of numerals CXXVIII. A quick look at a text book and this tells us were right
+
+Our problem child seem like 4 9 40 90 and 400 and 900 and 4000. SO lets hard code them in too. We can get creative later.
+
+So we know we need 1-10, 40, 50, 90, 100,400, 500, 900, 1000 and 4000.
+
+Let code this
